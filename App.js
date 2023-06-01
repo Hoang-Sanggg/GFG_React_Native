257982721c6a9f82ed6ebe86f2b676c6165d2d63
Quangtv208 <vingquang20082003@gmail.com>
2023-06-01 17:30:39 +0700

    update

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Screen37 from './src/views/screens/screen37';
+import Screen34 from './src/views/screens/screen34';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    // <View style={styles.container}>
+    //   <Text>Open up App.js to start working on your app!</Text>
+    //   <StatusBar style="auto" />
+    // </View>
+    
+    <Screen34></Screen34>
   );
 }
 
