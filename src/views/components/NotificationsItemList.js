69f88aaabe4865df4c746b2922dd90664ecbc2f1
tiin1705 <daonhattiin1705@gmail.com>
2023-06-01 18:35:39 +0700

    Create NotificationsItemList.js

---
+++ src/views/components/NotificationsItemList.js
@@ -0,0 +1,19 @@
+import React, {useState} from "react";
+import {Button, StyleSheet,Text, View, TextInput} from 'react-native';
+
+export default function List(){
+    
+    return (
+        <View style = {style.container}>
+
+        </View>
+
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    }
+
+});
