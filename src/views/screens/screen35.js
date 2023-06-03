583ae07049c4bfe669de1c1337999d50e37dc6b2
Quangtv208 <vingquang20082003@gmail.com>
2023-06-04 01:50:38 +0700

    updata screen 35 37 34 10

--- src/views/screens/screen35.js
+++ src/views/screens/screen35.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {View, FlatList, Text, StyleSheet, Animated, Pressable} from 'react-native'
-import { Picker } from '@react-native-picker/picker';
+
 
 const data = [30, 40, 50, 60, 70, 80, 90, 100, 101, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127, 128, 129, 130, 131, 132, 133, 134, 135, 136, 137, 138, 139, 140, 141,];
 
