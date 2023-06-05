2fa60fb9346fa331f8fdc746e49dd438ead88aff
tiin1705 <daonhattiin1705@gmail.com>
2023-06-05 12:19:06 +0700

    Create screen7.js

---
+++ src/views/screens/screen7.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { View } from "react-native/types";
+const screen7 = ()=>{
+    return(
+        <View
+            style={{
+                backgroundColor: '#1E1E1E',
+                flex: 1,
+            }}>
+            <View
+            style={{
+                flex: 1,
+                margin: '0 auto',
+            }}>
+                <Text>Daily Workout Schedule</Text>
+            </View>
+        </View>    
+      
+    
+
+     
+    )
+}
+export default screen7;
